b4ac8990f9fdd1ccb5b7d2c10e037bab438cf209
har8360 <har8360@hotmail.com>
2020-02-10 23:17:18 -0600

    Created ECOMMERCE-APP project with react app and added files for the HomePage component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HomePage from './homepage.component'
+import './App.css';
+
+
+function App() {
+  return (
+    <div>
+      Hello world
+    </div>
+  );
+}
+
+export default App;
---
+++ src/homepage.component.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './homepage.styles.scss';
+
+const HomePage = () => {
+    return(
+        <div className='homepage'>
+            Hello World
+        </div>
+    );
+}
+export default HomePage;
