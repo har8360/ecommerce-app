9790219825d520ad4eccdaa4db1bcec94d06d1e4
har8360 <har8360@hotmail.com>
2020-02-16 19:31:26 -0600

    modified directory component to add dummy linkUrls to the sections array, added withRouter to menu-item component to route to the HatsPage com ponent

--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom'
 import './menu-item.styles.scss';
 
-const MenuItem = ({title, imageUrl, size}) => (
-    <div style= {{backgroundImage: `url(${imageUrl})`}} className={`${size} menu-item`}>
+const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => (
+    <div 
+    style= {{backgroundImage: `url(${imageUrl})`}} 
+    className={`${size} menu-item`}
+    onClick={()=>history.push(`${match.url}${linkUrl}`)}
+    >
         <div className='background-image'style={{backgroundImage: `url(${imageUrl})`}}/>
         <div className='content'>
             <h1 className='title'>{title.toUpperCase()}</h1>
@@ -11,4 +16,4 @@ const MenuItem = ({title, imageUrl, size}) => (
   </div>
 )
 
-export default MenuItem;
+export default withRouter(MenuItem);
